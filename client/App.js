4f6b0801dfe95db11e9fc3f1943cec3a921c0898
Amir thapa magar <amirthapa2019@gmail.com>
2022-11-07 21:10:13 +0545

    feat:create database

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Router from "./components/Router";
+
+const App = () => (
+  <>
+    <NavBar />
+    <Router />
+  </>
+);
+
+export default App;
